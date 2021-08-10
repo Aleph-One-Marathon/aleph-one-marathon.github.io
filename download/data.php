e4d0dfc877e8d06679f4775ce1cba6f271bdd203
Hopper262 <hopper@whpress.com>
2021-08-10 00:22:19 -0400

    Avoid PHP warnings

--- download/data.php
+++ download/data.php
@@ -1,17 +1,19 @@
 ---
 ---
-<?php 
+<?php
 
 {% capture dummy %}{% include version_info.md %}{% endcapture %}
 
-$game = $_GET['game'];
 $link = '/';
-if ($game == 'marathon') {
-  $link = '{{ link_m1data }}';
-} else if ($game == 'marathon2') {
-  $link = '{{ link_m2data }}';
-} else if ($game == 'infinity') {
-  $package = '{{ link_m3data }}';
+if (isset($_GET['game'])) {
+  $game = $_GET['game'];
+  if ($game == 'marathon') {
+    $link = '{{ link_m1data }}';
+  } else if ($game == 'marathon2') {
+    $link = '{{ link_m2data }}';
+  } else if ($game == 'infinity') {
+    $package = '{{ link_m3data }}';
+  }
 }
 
 header('Location: ' . $link);
--- download/windows.php
+++ download/windows.php
@@ -1,29 +1,34 @@
 ---
 ---
-<?php 
+<?php
 
 {% capture dummy %}{% include version_info.md %}{% endcapture %}
 
-$game = $_GET['game'];
-$link = '{{ link_alephmac }}';
-if ($game == 'marathon') {
-  $link = '{{ link_m1win }}';
-} else if ($game == 'marathon2') {
-  $link = '{{ link_m2win }}';
-} else if ($game == 'infinity') {
-  $link = '{{ link_m3win }}';
+$link = '{{ link_alephwin }}';
+
+if (isset($_GET['game'])) {
+  $game = $_GET['game'];
+  if ($game == 'marathon') {
+    $link = '{{ link_m1win }}';
+  } else if ($game == 'marathon2') {
+    $link = '{{ link_m2win }}';
+  } else if ($game == 'infinity') {
+    $link = '{{ link_m3win }}';
+  }
 }
 
-$type = $_GET['type'];
-if ($type == 'source') {
-  $link = '{{ link_source }}';
-} else if ($type == 'upgrade') {
-  if ($game == 'marathon') {
-    $link = '{{ link_m1exe }}';
-  } else if ($game == 'marathon2') {
-    $link = '{{ link_m2exe }}';
-  } else if ($game == 'infinity') {
-    $link = '{{ link_m3exe }}';
+if (isset($_GET['type'])) {
+  $type = $_GET['type'];
+  if ($type == 'source') {
+    $link = '{{ link_source }}';
+  } else if ($type == 'upgrade') {
+    if ($game == 'marathon') {
+      $link = '{{ link_m1exe }}';
+    } else if ($game == 'marathon2') {
+      $link = '{{ link_m2exe }}';
+    } else if ($game == 'infinity') {
+      $link = '{{ link_m3exe }}';
+    }
   }
 }
 
